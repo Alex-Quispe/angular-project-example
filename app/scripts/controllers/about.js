cb5734088478ae95fa71b34f1447cd70ef919451
waquispe <q.wilmer.alex@gmail.com>
2016-10-22 17:21:46 -0400

    Project structure created

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularProjectExampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularProjectExampleApp
+ */
+angular.module('angularProjectExampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularProjectExampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularProjectExampleApp
+ */
+angular.module('angularProjectExampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
